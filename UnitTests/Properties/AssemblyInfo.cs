c21a2303225cbcb1289b921dc44d9cd903ed3b19
Brendon Gomes <brendongomes40@gmail.com>
2025-06-02 00:15:29 -0300

    Atualiza solução e adiciona projeto de testes unitários
    - Adiciona o projeto "UnitTests".
    - Adiciona `UnitTests.csproj` com configurações e dependências do MSTest.
    - Implementa `UtilsClassTest.cs` com métodos de teste para funcionalidades utilitárias.

---
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ca08bb02-099e-41d0-8564-69e8f1a284e6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTests/UtilsClassTest.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using static FatecDemandaDownloaderNetFramework.Utils;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UtilsClassTest
+    {
+        [DataTestMethod]
+        [DataRow("engenharia de produção", "Engenharia De Produção")]
+        [DataRow("ADMINISTRAÇÃO", "Administração")]
+        [DataRow("sistemas para internet", "Sistemas Para Internet")]
+        public void CapitalizeText_ShouldReturnCorrectTitleCase(string input, string expected)
+        {
+            var result = CapitalizeText(input);
+            Assert.AreEqual(expected, result);
+        }
+
+        [DataTestMethod]
+        [DataRow("mauá", "Fatec Mauá")]
+        [DataRow(" FATEC Zona Leste ", "Fatec Zona Leste")]
+        [DataRow("fatec santos", "Fatec Santos")]
+        public void ParseFatecName_ShouldFormatProperly(string input, string expected)
+        {
+            var result = ParseFatecName(input);
+            Assert.AreEqual(expected, result);
+        }
+
+        [DataTestMethod]
+        [DataRow("São Paulo", "SAO PAULO")]
+        [DataRow("Fatec São Caetano", "FATEC SAO CAETANO")]
+        [DataRow("Zona Leste", "ZONA LESTE")]
+        public void NormalizeText_ShouldRemoveAccentsAndToUpper(string input, string expected)
+        {
+            var result = NormalizeText(input);
+            Assert.AreEqual(expected, result);
+        }
+
+        [DataTestMethod]
+        [DataRow(0, 0, 300, "300ms")]
+        [DataRow(0, 5, 100, "5s 100ms")]
+        [DataRow(2, 30, 50, "2m 30s 50ms")]
+        public void FormatElapsedTime_ShouldReturnFormattedTime(int minutes, int seconds, int milliseconds, string expected)
+        {
+            var time = new TimeSpan(0, 0, minutes, seconds, milliseconds);
+            var result = FormatElapsedTime(time);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
